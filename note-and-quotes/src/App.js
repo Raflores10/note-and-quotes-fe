b848fff75b30bc3c63eb0aacd177ef385254d08a
kraigmansfield <Kraig.mansfield@gmail.com>
2023-03-19 10:03:07 -0700

    added login/register. Need to move to their own pages? Or put into a modal?

--- note-and-quotes/src/App.js
+++ note-and-quotes/src/App.js
@@ -2,16 +2,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navigation from "./components/Navigation";
 import Profile from "./pages/Profile";
+import { Login } from "./pages/Login";
+import Register from "./pages/Register";
+import React, {useState} from 'react';
 
 function App() {
-  return( 
+  const [currentForm, setCurrentForm] = useState('login');
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+  return(
+    <>
+    <div className="App">
+      {
+        currentForm === 'login' ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+      }
+    </div>
 
   <BrowserRouter>
     <Navigation />
 
       <Routes>
         <Route path="/profile/" element={<Profile/>}/>
-        <Route />
+        <Route path="/login/" element={<Login/>}/>
         <Route />
         <Route />
 
@@ -19,7 +32,7 @@ function App() {
 
       </Routes>
     </BrowserRouter>
-    
+    </>
 )};
 
 export default App;
